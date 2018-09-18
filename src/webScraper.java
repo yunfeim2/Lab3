fe97d1801c4fb407aafb45244326ae5a4404e091
yunfeim2 <42976501+yunfeim2@users.noreply.github.com>
2018-09-19 01:36:09 +0800

    mnnjnj

--- src/webScraper.java
+++ src/webScraper.java
@@ -1,2 +1,42 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
+
 public class webScraper {
+
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+    }
+
+    /**
+     *
+     */
+    public static int numberOfOcurrence(final String word, final String stringContents) {
+        String stringContents1 = urlToString(stringContents) ;
+        int number = 0;
+        while (stringContents1.indexOf(word) != -1) {
+            stringContents1 = stringContents.substring(0,stringContents1.indexOf(word)) + stringContents1.substring(stringContents.indexOf(word) + word.length());
+            number++;
+        }
+        return number;
+    }
+
+    public static void main(String[] arg) {
+        System.out.println(numberOfOcurrence("good","http://erdani.com/tdpl/hamlet.txt"));
+    }
 }
